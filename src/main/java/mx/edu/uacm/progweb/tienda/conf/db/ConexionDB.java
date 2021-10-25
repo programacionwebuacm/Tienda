0ccacc76af8115b7932a1ee9f0ceeadb8a572ca8
mapeouacm <prog.web.uacm@gmail.com>
2021-10-25 12:46:13 -0500

    sin conexion
    sin conexón

--- src/main/java/mx/edu/uacm/progweb/tienda/conf/db/ConexionDB.java
+++ src/main/java/mx/edu/uacm/progweb/tienda/conf/db/ConexionDB.java
@@ -7,10 +7,10 @@ import java.sql.SQLException;
 public class ConexionDB {
 	
 	public static Connection con;
-	public static String base_datos_nombre = "d9i2uvjudfm7e1";
-	public static String usuario = "vxrndkgxiuzvhg";
-	public static String passw = "b4ddce1fa0f99b1372467d43bff725fe65c1903ea5ffa77236ba56b7d9862945";
-	public static String url = "jdbc:postgresql://ec2-52-70-107-254.compute-1.amazonaws.com:5432/" + base_datos_nombre;
+	public static String base_datos_nombre = "";
+	public static String usuario = "";
+	public static String passw = "";
+	public static String url = "" + base_datos_nombre;
 	
 	//More Unit
 	//Ctrl + J
